7fc24b1b88ae7e4890b43eb9755d12423869228e
Maxim Tkachenko <maximus19960411@gmail.com>
2023-05-27 21:23:30 +0300

    On branch master Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ def roll_dice(max_val):
     return random.randint(1, max_val)
 
 
-T_dice = [4, 6, 8, 10, 12, 20, 100]  # Варианты кубиков
+T_dice = [2, 4, 6, 8, 10, 12, 20, 100]  # Варианты кубиков
+print("There are these types of dice: ", T_dice)
 print("What dice are you roll?")
 while True:
     try:
         N_dice = int(input())  # Вводим нужный кубик
-        if N_dice not in T_dice:
-            print("There is no such dice in dnd, but as you say")
-        print(roll_dice(N_dice))
+        if N_dice <= 100:
+            if N_dice not in T_dice:
+                print("There is no such dice in dnd, but as you say")
+            print("Result is", roll_dice(N_dice))
+        else:
+            print("To much, 100 and no more")
     except:
         print("Oops, this is not number")
     print("Do you want to roll again? (Yes/No)")
